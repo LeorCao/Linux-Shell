119ee45adab429c183d0a7c6f96c446d5d2b8311
Leor Cao <leor_cao@163.com>
2019-07-28 01:39:38 +0800

    修复 etcd 安装脚本的bug

--- Shell/etcd_install.sh
+++ Shell/etcd_install.sh
@@ -1,9 +1,10 @@
 # !bin/bash
 
-# 下载版本
+# 下载版本，如果要下载其他版本的 ETCD 修改版本号即可
 VERSION=v3.3.13
+INSTALL_DIR=etcd-${VERSION}-linux-amd64
 # 安装位置
-INSTALL_PATH=/opt/etcd-${VERSION}-linux-amd64
+INSTALL_PATH=/opt/${INSTALL_DIR}
 
 # github 下载地址
 GITHUB_DLOAD_URL=https://github.com/etcd-io/etcd/releases/download
@@ -14,11 +15,12 @@ DLOAD_URL=${GOOGLE_DLOAD_URL}
 
 # 移除安装位置，并重新创建新的安装文件夹
 sudo rm -rf ${INSTALL_PATH}
-sudo mkdir -P ${INSTALL_PATH}
+sudo mkdir -p ${INSTALL_PATH}
 
 # 下载 ETCD 安装包，并解压
-curl -L ${GOOGLE_DLOAD_URL}/${VERSION}/etcd-${VERSION}-linux-amd64.tar.gz | tar -xzvf - -C ${INSTALL_PATH} --strip-components=1
+sudo curl -L ${GOOGLE_DLOAD_URL}/${VERSION}/${INSTALL_DIR}.tar.gz | \
+    sudo tar -xzf - -C ${INSTALL_PATH} --strip-components=1
 
 # 检查 ETCD 版本
 ${INSTALL_PATH}/etcd --version
-ETCDCTL_API=3 /tmp/etcd-download-test/etcdctl version
+ETCDCTL_API=3 ${INSTALL_PATH}/etcdctl version
