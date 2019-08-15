ec25278c2359e682b6c6133fe27706b19ef9ff79
LeorCao <leor_cao@163.com>
2019-08-15 12:32:28 +0800

    修改 vscode 安装脚本

--- Shell/vscode_install.sh
+++ Shell/vscode_install.sh
@@ -3,45 +3,37 @@
 # import
 source ./common/file.sh
 
-desktopDir=/usr/share/applications/
-vsName=vs-code.tar.gz
+# 下载地址
+DLOAD_URL=https://update.code.visualstudio.com/latest/linux-x64/stable
+# 本地程序保存基础路径
+SAVE_PATH=/opt/VSCode-linux-x64/
+# 快捷方式保存路径
+DESKTOPDIR=/usr/share/applications/
 
-mkdir ~/Downloads
-
-cd ~/Downloads
-
-curl -L https://update.code.visualstudio.com/latest/linux-x64/stable -o ${vsName}
-
-if [ ! -e /opt/ ]; then
-    sudo mkdir /opt/
+# 检查基础存储路径是否存在，不存在则创建
+if [ ! -e ${SAVE_PATH} ]; then
+    sudo mkdir ${SAVE_PATH}
 fi
 
-sudo tar -xzvf ${vsName} -C /opt/
+# 下载文件并解压到基础路径，且去除一层文件夹
+sudo curl -L ${DLOAD_URL} | sudo tar -xzf - -C ${SAVE_PATH} --strip-components=1
 
-sudo rm -rf ${vsName}
+# 进入程序文件夹
+cd ${SAVE_PATH}/bin
 
-cd /opt/
-
-vscodeDir=$(getChildDir "VSCode")
-echo $vscodeDir
-
-cd "${vscodeDir}/bin"
-
-sudo chmod 755 code
-
-sudo touch vscode.desktop
-
-sudo chmod 777 vscode.desktop
-
-sudo echo '[Desktop Entry]
-Name=VSCode
-Type=Application
-Exec=/opt/VSCode-linux-x64/bin/code
-Icon=/opt/VSCode-linux-x64/resources/app/resources/linux/code.png
-Categories=Development' >vscode.desktop
-
-sudo mv vscode.desktop ${desktopDir}
-
-sudo chmod 755 ${desktopDir}vscode.desktop
+# 创建快捷方式
+if [ ! -e ${DESKTOPDIR}/vscode.desktop ]; then
+    sudo chmod 755 code
+    sudo touch vscode.desktop
+    sudo chmod 777 vscode.desktop
+    sudo echo '[Desktop Entry]
+    Name=VSCode
+    Type=Application
+    Exec=/opt/VSCode-linux-x64/bin/code
+    Icon=/opt/VSCode-linux-x64/resources/app/resources/linux/code.png
+    Categories=Development' >vscode.desktop
+    sudo mv vscode.desktop ${DESKTOPDIR}
+    sudo chmod 755 ${DESKTOPDIR}vscode.desktop
+fi
 
 exit
