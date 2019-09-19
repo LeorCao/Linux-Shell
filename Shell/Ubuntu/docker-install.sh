f8b0e5439b0772f2032b7ac7cfdb3856f1e0ab60
LeorCao <leor_cao@163.com>
2019-09-19 16:44:11 +0800

    修改完成 Ubuntu docker 安装配置脚本

--- Shell/Ubuntu/docker-install.sh
+++ Shell/Ubuntu/docker-install.sh
@@ -3,17 +3,29 @@
 # Ubuntu 安装 docker 并配置阿里云镜像加速。
 
 DOCKER_URL=https://get.docker.com/
-DAEMON_FILE=/etc/docker/daemon.json
+DAEMON_DIR=/etc/docker/
+DAEMON_FILE_NAME=daemon.json
+DAEMON_PATH=${DAEMON_DIR}${DAEMON_FILE_NAME}
+
+if [ ! -e ~/Downloads ]; then
+    mkdir ~/Downloads
+fi
+
+cd ~/Downloads
+
+# 判断是否已经存在 daemon.json 文件，如果存在则将其重命名为备份文件。
+if [ ! -e ${DAEMON_PATH} ]; then
+    sudo mv  ${DAEMON_PATH}-backup
+fi
 
 # 安装 docker
 wget -qO- ${DOCKER_URL} | sh
-
 # 修改镜像源配置
-sudo touch ${DAEMON_FILE}
-
-sudo echo '{
+touch ${DAEMON_FILE_NAME}
+echo '{
   "registry-mirrors": ["https://odzkltf1.mirror.aliyuncs.com"]
-}' > ${DAEMON_FILE}
+}' > ${DAEMON_FILE_NAME}
+sudo mv ${DAEMON_FILE_NAME} ${DAEMON_PATH}
 
 # 配置权限，解决 docker 需要 sudo。
 sudo groupadd docker
@@ -21,4 +33,3 @@ sudo gpasswd -a ${USER} docker
 # 重启 docker 服务
 sudo systemctl daemon-reload
 sudo systemctl restart docker
-newgrp - docker
